eb4f387c79e1e940701f07b025b5ae164161433d
Lukasz <lukaszrybicki5@gmail.com>
2019-08-26 17:56:14 +0200

    26.08

--- Sklep/src/main/java/com/bookstore/controller/HomeController.java
+++ Sklep/src/main/java/com/bookstore/controller/HomeController.java
@@ -1,15 +1,28 @@
 package com.bookstore.controller;
 
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@ComponentScan
 public class HomeController {
 
-@RequestMapping("/")
-    public String index(){
-    return "index";
-}
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/myAccount")
+    public String myAccount() {
+        return "myAccount";
+    }
+
+
+    @RequestMapping("/dupa")
+    public String mydupa() {
+        return "dupa";
+    }
 
 }
